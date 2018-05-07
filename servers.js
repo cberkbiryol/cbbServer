c0f57926dc6ca3cb53a31cb78b85a9bae0029df4
Cemal Biryol <cbbiryol@Cemals-MacBook-1774.local>
2018-05-07 19:38:58 -0400

    Server done

--- servers.js
+++ servers.js
@@ -1,21 +1,27 @@
 var http = require("http");
 
-function reqHandle1(req,resp) {
-    resp.end("Something good Works!! Path Hit: " + req.url);
+var arr= ["aaa","bbb","addd"]
+var PORT1=7000;
+var PORT2=7500;
+
+// Request Handlers
+function reqHandle1(req,res) {
+    var ai= Math.floor(Math.random()*arr.length)
+    res.end("Something good Works!! " + arr[ai] +" Path Hit: " + req.url);
 }
 
-function reqHandle2(req,resp) {
-    resp.end("Something bad Works!! Path Hit: " + req.url);
+function reqHandle2(req,res) {
+    res.end("Something bad Works!! Path Hit: " + req.url);
 }
 
-PORT1=7000;
-PORT2=7500;
+// Servers
 server1 = http.createServer(reqHandle1);
+server2 = http.createServer(reqHandle2);
+
+// Listeners
 server1.listen(PORT1,function(){
     console.log("Server listening on: http://localhost:" + PORT1);
 })
-
-server2 = http.createServer(reqHandle2)
 server2.listen(PORT2,function(){
     console.log("Server listening on: http://localhost:" + PORT2);
 })
